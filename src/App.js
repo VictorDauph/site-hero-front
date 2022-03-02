8def88706212d5972e0e47d2f9b6d7055eb632fe
VictorDauph <victor.dauph@gmail.com>
2022-03-02 17:44:33 +0100

    modif icone et titre

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // App.js sert à gérer le routing des pages à afficher
 import {Route, Routes} from "react-router-dom"; //Route est l'élément qui gère les Routes pour le différentes pages à afficher. Switch permet de n'afficher qu'une page à la fois.
 import MockupPage from "./pages/mockupPage";
-import Header from "./components/header";
+
 
 function App() {
   return (
