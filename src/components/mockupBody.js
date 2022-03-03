f892e374d5fc9dca6c1c728ca1b75a64ae3b9a7b
VictorDauph <victor.dauph@gmail.com>
2022-03-03 14:46:01 +0100

    email table styling

--- src/components/mockupBody.js
+++ src/components/mockupBody.js
@@ -1,7 +1,34 @@
 function MockupBody(){
     return(
-        <main className="">
-            Mockup Body
+        <main className="container my-2">
+            <table className="tableMail shadow">
+                {/* Table header*/}
+                <thead >
+                    <tr>
+                        <th>Expediteur</th>
+                        <th>Objet</th>
+                        <th>date</th>
+                    </tr>
+                </thead>
+                {/* Table body*/}
+                <tbody>
+                    <tr >
+                        <td>Mark</td>
+                        <td>coucou</td>
+                        <td>01/01/01</td>
+                    </tr>
+                    <tr >
+                        <td>Jacob</td>
+                        <td>ceci n'est pas un virus</td>
+                        <td>01/01/01</td>
+                    </tr>
+                    <tr>
+                        <td >Larry the Bird</td>
+                        <td >Ta daronne est hot!</td>
+                        <td>01/01/01</td>
+                    </tr>
+                </tbody>
+            </table>
         </main>
     )
 }
