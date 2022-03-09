ea7a33b516a05e5d46791b9085c7991ed2bc985b
VictorDauph <victor.dauph@gmail.com>
2022-03-09 21:17:03 +0100

    mise en page email content

--- src/components/EmailBody.js
+++ src/components/EmailBody.js
@@ -4,7 +4,7 @@ import { EmailContext } from "../contexts/EmailContext"
 function EmailBody(){
     const EmailCtx=useContext(EmailContext)
     return(
-        <div>
+        <div className="container">
             <h2>Email Body </h2>
             <p>{EmailCtx.loadedEmail.content}</p>
         </div>
